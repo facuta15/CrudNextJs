21981fb69fad96e1c14a5678274c2b7bf4740b1b
facundotadini-admin <facundo@activity.com.ar>
2023-08-17 10:50:09 -0300

    initial commit

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,12 @@
+
+import { BiAddToQueue } from "react-icons/bi";
+
+const AddTask = () => {
+  return (
+    <div>
+        <button className="btn btn-primary w-50">Add Task <BiAddToQueue/></button>
+    </div>
+  )
+}
+
+export default AddTask
